041ecfe5bde8ae7511be80f1ae0775a36b1931cd
ihagedo <isaachagedorn@gmail.com>
2023-01-27 14:19:26 -0500

    added environment handler

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,20 @@ use std::{env, io};
 
 use actix_web::{middleware, App, HttpServer};
 
+use dotenv::dotenv;
+use listenfd::ListenFd;
+
 mod end_point;
 mod constants;
 mod response;
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
-    env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+    //env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
     env_logger::init();
 
-    HttpServer::new(|| {
+    let mut listenfd = ListenFd::from_env();
+    let mut server = HttpServer::new(|| {
         App::new()
 
             .wrap(middleware::Logger::default())
@@ -23,8 +27,14 @@ async fn main() -> io::Result<()> {
             .service(end_point::get)
             .service(end_point::create)
             .service(end_point::delete)
-    })
-    .bind("0.0.0.0:9090")?
-    .run()
-    .await
+    });
+    
+    server = match listenfd.take_tcp_listener(0)? {
+        Some(listener) => server.listen(listener)?,
+        None => {
+            let host = env::var("HOST").expect("Please set host in .env");
+            let port = env::var("PORT").expect("Please set port in .env");
+            server.bind(format!("{}:{}", host, port))?
+        }
+    };
 }
