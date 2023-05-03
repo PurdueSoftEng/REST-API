cf9e37b05a57a5d61131c730b26998e1210db3be
Colleen Granelli <colleengranelli@pal-nat186-188-175.itap.purdue.edu>
2023-05-03 11:57:55 -0400

    sample update

--- src/tests.rs
+++ src/tests.rs
@@ -9,7 +9,7 @@ fn hello_world() {
     let client = Client::new(rocket()).expect("valid rocket instance");
     let mut response = client.get("/hello").dispatch();
     assert_eq!(response.status(), Status::Ok);
-    assert_eq!(response.body_string(), Some("Hello, world!".into()));
+    assert_eq!(response.body_string(), Some("Hello, world".into()));
 }
 
 #[test]
